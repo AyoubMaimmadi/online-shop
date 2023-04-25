524bc3ce6010901ab7aa8ac25526ce703a2a715b
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-04-25 11:14:47 +0100

    new

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,7 +1,25 @@
+import Link from 'next/link'
 import React from 'react'
 
 const HeroBanner = () => {
-  return <div>HeroBanner</div>
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">Cerramics</p>
+        <h3>mid</h3>
+        <img src="" alt="ceramic" className="hero-banner-image" />
+        <div>
+          <Link href="/product/ID">
+            <button type="button">Button text</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>description</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default HeroBanner
