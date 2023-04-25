d0399c6fe71cc2595f5b4d04e7f2cf70e132ce6b
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-04-25 17:19:25 +0100

    new

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>Cerramics Store</title>
+        <title>Ceramics Store</title>
       </Head>
       <header>
         <NavBar />
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import Link from 'next/link'
+import { AiOutlineShopping } from 'react-icons/ai'
 
 const NavBar = () => {
-  return <div>NavBar</div>
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Ceramics of Morocco</Link>
+      </p>
+
+      <button className="cart-icon" type="button" onClick="">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">2</span>
+      </button>
+    </div>
+  )
 }
 
 export default NavBar
