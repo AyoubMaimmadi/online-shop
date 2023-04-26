9220344327bf8786f3bfa1059a25dda33de36d4b
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-04-26 11:35:41 +0100

    new

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
-import '../styles/globals.css'
-import { Layout } from '../components'
+import React from 'react'
+import { Toaster } from 'react-hot-toast'
 
-export default function App({ Component, pageProps }) {
+import { Layout } from '../components'
+import '../styles/globals.css'
+import { StateContext } from '../context/StateContext'
+
+function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   )
 }
+
+export default MyApp
