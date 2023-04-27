3b38bfbf27e5af0272f578849431b1afc0551f37
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-04-27 15:27:57 +0100

    new

--- components/index.js
+++ components/index.js
@@ -3,7 +3,7 @@ import { default as Footer } from './Footer'
 import { default as FooterBanner } from './FooterBanner'
 import { default as HeroBanner } from './HeroBanner'
 import { default as Layout } from './Layout'
-import { default as Navbar } from './Navbar'
+import { default as NavBar } from './NavBar'
 import { default as Product } from './Product'
 
 export { Cart, Footer, FooterBanner, HeroBanner, Layout, Navbar, Product }
