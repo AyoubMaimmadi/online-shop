fcbabfbe47a7bd575f4fb313b3385e5064a5169b
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2023-04-27 14:33:58 +0100

    new

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -10,7 +10,10 @@ export default async function handler(req, res) {
         mode: 'payment',
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
-        shipping_options: [{ shipping_rate: 'shr_1N1TFHBpl9c3b00faZ4smQ2g' }],
+        shipping_options: [
+          { shipping_rate: 'shr_1N1TFHBpl9c3b00faZ4smQ2g' },
+          { shipping_rate: 'shr_1N1UpTBpl9c3b00fjNhhnlQb' },
+        ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref
           const newImage = img
@@ -36,8 +39,8 @@ export default async function handler(req, res) {
             quantity: item.quantity,
           }
         }),
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
       }
 
       // Create Checkout Sessions from body params.
